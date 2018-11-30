695d65fc3f85a48ff4d124f67c21e59b4371eeb8
Daniel Oliveira <danielso2007@gmail.com>
2018-11-30 12:21:49 -0200

    feat: ApolloClient: Testando comunicação por meio de Queries.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,58 +1,34 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {Component, OnInit} from '@angular/core';
+import {Apollo} from 'apollo-angular';
+import gql from 'graphql-tag';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  private apiUrl = 'https://api.graph.cool/simple/v1/cjp34sj3p82vz0178reis2tdf';
+  loading = true;
 
-  constructor(private http: HttpClient) {
-    // this.createUser();
-    this.allUsers();
-  }
+  constructor(private apollo: Apollo) {}
 
-  allUsers(): void {
-    const body = {
-      query: `
-        query {
-          allUsers {
-            id
-            name
-            email
+  ngOnInit() {
+    this.apollo
+      .query({
+        query: gql`
+          {
+            allUsers {
+              id
+              name
+              email
+            }
           }
-        }
-      `
-    };
-
-    this.http.post(this.apiUrl, body).subscribe(res => {
-      console.log(res);
-    });
-  }
-
-  createUser(): void {
-    const body = {
-      query: `
-        mutation CreatenewUser($name: String!, $email: String!, $password: String!) {
-          createUser(name: $name, email: $email, password: $password) {
-            id
-            name
-            email
-          }
-        }
-      `,
-      variables: {
-        name: 'Black Panther',
-        email: 'panther@email.com',
-        password: '123456'
-      }
-    };
-
-    this.http.post(this.apiUrl, body).subscribe(res => {
-      console.log('Mutations: ', res);
-    });
+        `,
+      })
+      .subscribe(result => {
+        this.loading = result.loading;
+        console.log(result);
+      });
   }
 }

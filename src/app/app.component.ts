abdb0fa92c13d0d68e168f1d92c42375102ca1e4
Daniel Oliveira <danielso2007@gmail.com>
2018-11-30 12:31:26 -0200

    feat: ApolloClient: Criando novo registro com Mutations.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {Apollo} from 'apollo-angular';
 import gql from 'graphql-tag';
+import { Subscription } from 'apollo-client/util/Observable';
+import { Subscribable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +16,12 @@ export class AppComponent implements OnInit {
   constructor(private apollo: Apollo) {}
 
   ngOnInit() {
+    // this.createUser().subscribe(res => {
+      this.allUser();
+    // });
+  }
+
+  allUser(): void {
     this.apollo
       .query({
         query: gql`
@@ -29,6 +37,26 @@ export class AppComponent implements OnInit {
       .subscribe(result => {
         this.loading = result.loading;
         console.log(result);
+      });
+  }
+
+  createUser(): Subscribable<any> {
+    return this.apollo
+      .mutate({
+        mutation: gql`
+          mutation CreateNewUser($name: String!, $email: String!, $password: String!) {
+            createUser(name: $name, email: $email, password: $password) {
+              id
+              name
+              email
+            }
+          }
+        `,
+        variables: {
+          name: 'Iron Man',
+          email: 'ironman@email.com',
+          password: '123456'
+        }
       });
   }
 }

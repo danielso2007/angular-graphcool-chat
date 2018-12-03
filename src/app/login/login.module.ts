34a555ff6fd82792976950fa4aac6da00bc35cb8
danielso2007 <danielso2007@gmail.com>
2018-12-02 23:18:12 -0200

    feat: Criando a tela de login.

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 
 import { LoginRoutingModule } from './login-routing.module';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent],
   imports: [
     SharedModule,
     LoginRoutingModule
+  ],
+  exports: [
+    LoginComponent
   ]
 })
 export class LoginModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
 @NgModule({
   exports: [
-    CommonModule
-  ]
+    CommonModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule
+  ],
+  declarations: []
 })
 export class SharedModule { }

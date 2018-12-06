59a11b2db7b0b2eeee46d5654728f094ed2daa17
Daniel Oliveira <danielso2007@gmail.com>
2018-12-06 15:54:20 -0200

    feat: Atualizando o statdo de login nas chamadas REST.

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Observable, throwError, ReplaySubject } from 'rxjs';
 import { Apollo } from 'apollo-angular';
-import { AUTHENTICATE_USER_MUTATION, SIGNUP_USER_MUTATION } from './auth.graphql';
-import { catchError, map } from 'rxjs/operators';
+import {
+  AUTHENTICATE_USER_MUTATION,
+  SIGNUP_USER_MUTATION
+} from './auth.graphql';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
   private _isAuthenticated = new ReplaySubject<boolean>(1);
 
   constructor(private apollo: Apollo) {
@@ -31,7 +33,7 @@ export class AuthService {
   signinUser(variables: {
     email: string;
     password: string;
-  }): Observable<{ id: string; token: string, __typename: string }> {
+  }): Observable<{ id: string; token: string; __typename: string }> {
     return this.apollo
       .mutate({
         mutation: AUTHENTICATE_USER_MUTATION,
@@ -39,15 +41,19 @@ export class AuthService {
       })
       .pipe(
         map(res => res.data.authenticateUser),
-        catchError(this.handleError)
+        tap(res => this.setAuthState({id: res && res.id, token: res && res.token, isAuthenticated: res !== null})),
+        catchError(error => {
+          this.setAuthState({id: null, token: null, isAuthenticated: false});
+          return throwError(error);
+        })
       );
   }
 
   signupUser(variables: {
-    name: String,
+    name: String;
     email: string;
     password: string;
-  }): Observable<{ id: string, token: string, __typename: string }> {
+  }): Observable<{ id: string; token: string; __typename: string }> {
     return this.apollo
       .mutate({
         mutation: SIGNUP_USER_MUTATION,
@@ -55,12 +61,34 @@ export class AuthService {
       })
       .pipe(
         map(res => res.data.signupUser),
-        catchError(this.handleError)
+        tap(res => this.setAuthState({id: res && res.id, token: res && res.token, isAuthenticated: res !== null})),
+        catchError(error => {
+          this.setAuthState({id: null, token: null, isAuthenticated: false});
+          return throwError(error);
+        })
       );
   }
 
   handleError(error: any): Observable<any> {
     console.error('ERRO NA REQUISIÇAO => ', error);
     return throwError(error);
+  }
+
+  private setAuthState(
+    authData: { id: string; token: string; isAuthenticated: boolean }, isRefresh: boolean = false): void {
+    if (authData.isAuthenticated) {
+      // window.localStorage.setItem(StorageKeys.AUTH_TOKEN, authData.token);
+      // this.setAuthUser(authData.id)
+      //   .pipe(
+      //     take(1),
+      //     tap(() => this._isAuthenticated.next(authData.isAuthenticated))
+      //   )
+      //   .subscribe();
+      // if (!isRefresh) {
+      //   this.apolloConfigModule.closeWebSocketConnection();
+      // }
+      // return;
+    }
+    this._isAuthenticated.next(authData.isAuthenticated);
   }
 }

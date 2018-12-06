1cd0563fd83cd705da12f9712a2a69f8d22e7d85
Daniel Oliveira <danielso2007@gmail.com>
2018-12-06 20:21:29 -0200

    feat: AutoLoginGuard com CanActivate para rota de login.

---
+++ src/app/login/auto-login.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+
+import { AuthService } from '../core/services/auth.service';
+
+@Injectable()
+export class AutoLoginGuard implements CanActivate {
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+    return this.authService.isAuthenticated
+      .pipe(
+        tap(is => (is) ? this.router.navigate(['/dashboard']) : null),
+        map(is => !is)
+      );
+  }
+
+}

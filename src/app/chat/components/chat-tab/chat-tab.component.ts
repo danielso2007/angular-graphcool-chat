66c86bb4afbca3c696ba67595b3a0d9a2c9d1929
Daniel Oliveira <danielso2007@gmail.com>
2018-12-12 11:46:44 -0200

    feat: Configurando rotas e router outlet para lista de usuários e chats.

--- src/app/chat/components/chat-tab/chat-tab.component.ts
+++ src/app/chat/components/chat-tab/chat-tab.component.ts
@@ -4,9 +4,10 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-chat-tab',
   template: `
     <nav mat-tab-nav-bar backgroundColor="primary">
-    <a mat-tab-link>Chats</a>
-    <a mat-tab-link>Users</a>
+      <a mat-tab-link routerLink="./" routerLinkActive #chatRla="routerLinkActive" [active]="chatRla.isActive" [routerLinkActiveOptions]="{exact: true}">Chats</a>
+      <a mat-tab-link routerLink="users" routerLinkActive #userRla="routerLinkActive" [active]="userRla.isActive">Users</a>
     </nav>
+    <router-outlet></router-outlet>
   `
 })
 export class ChatTabComponent {

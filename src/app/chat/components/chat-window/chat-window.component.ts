b341440fd33c9901681a8db6d006d6a218703c5c
Daniel Oliveira <danielso2007@gmail.com>
2018-12-12 18:24:55 -0200

    feat: Capturando o Chat no ChatWindowComponent.

--- src/app/chat/components/chat-window/chat-window.component.ts
+++ src/app/chat/components/chat-window/chat-window.component.ts
@@ -1,19 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Chat } from '../../models/chat.model';
+import { Subscription } from 'apollo-client/util/Observable';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-chat-window',
   templateUrl: './chat-window.component.html',
   styleUrls: ['./chat-window.component.scss']
 })
-export class ChatWindowComponent implements OnInit {
+export class ChatWindowComponent implements OnInit, OnDestroy {
+
+  chat: Chat;
+  private subscriptions: Subscription[] = [];
 
   constructor(
     private router: ActivatedRoute
   ) { }
 
   ngOnInit() {
-    this.router.data.subscribe(console.log);
+    this.router.data
+    .pipe(
+      map(routerData => this.chat = routerData.chat)
+    )
+    .subscribe();
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(s => s.unsubscribe());
   }
 
 }

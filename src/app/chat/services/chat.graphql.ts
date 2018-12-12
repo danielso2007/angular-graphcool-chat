131eb315dd23379f47e544fe64ae359c0ed34fc3
Daniel Oliveira <danielso2007@gmail.com>
2018-12-12 17:29:47 -0200

    feat: Método no ChatService para buscar lista de chats.

---
+++ src/app/chat/services/chat.graphql.ts
@@ -0,0 +1,152 @@
+import { FileFragment } from './../../core/services/file.graphql';
+import gql from 'graphql-tag';
+
+import { Chat } from '../models/chat.model';
+
+export interface AllChatsQuery {
+  allChats: Chat[];
+}
+
+export interface ChatQuery {
+  Chat: Chat;
+}
+
+const ChatFragment = gql`
+  fragment ChatFragment on Chat {
+    id
+    title
+    createdAt
+    isGroup
+    photo {
+      ...FileFragment
+    }
+    users(
+      first: 1,
+      filter: {
+        id_not: $loggedUserId
+      }
+    ) {
+      id
+      name
+      email
+      createdAt
+      photo {
+        ...FileFragment
+      }
+    }
+  }
+  ${FileFragment}
+`;
+
+const ChatMessagesFragment = gql`
+  fragment ChatMessagesFragment on Chat {
+    messages(
+      last: 1
+    ) {
+      id
+      createdAt
+      text
+      sender {
+        id
+        name
+      }
+    }
+  }
+`;
+
+export const USER_CHATS_QUERY = gql`
+  query UserChatsQuery($loggedUserId: ID!) {
+    allChats(
+      filter: {
+        users_some: {
+          id: $loggedUserId
+        }
+      }
+    ) {
+      ...ChatFragment
+      ...ChatMessagesFragment
+    }
+  }
+  ${ChatFragment}
+  ${ChatMessagesFragment}
+`;
+
+export const CHAT_BY_ID_OR_BY_USERS_QUERY = gql`
+  query ChatByIdOrByUsersQuery($chatId: ID!, $loggedUserId: ID!, $targetUserId: ID!) {
+
+    Chat(
+      id: $chatId
+    ) {
+      ...ChatFragment
+    }
+
+    allChats(
+      filter: {
+        AND: [
+          { users_some: { id: $loggedUserId } },
+          { users_some: { id: $targetUserId } }
+        ],
+        isGroup: false
+      }
+    ) {
+      ...ChatFragment
+    }
+
+  }
+  ${ChatFragment}
+`;
+
+export const CREATE_PRIVATE_CHAT_MUTATION = gql`
+  mutation CreatePrivateChatMutation($loggedUserId: ID!, $targetUserId: ID!) {
+    createChat(
+      usersIds: [
+        $loggedUserId,
+        $targetUserId
+      ]
+    ) {
+      ...ChatFragment
+      ...ChatMessagesFragment
+    }
+  }
+  ${ChatFragment}
+  ${ChatMessagesFragment}
+`;
+
+export const CREATE_GROUP_MUTATION = gql`
+  mutation CreateGroupMutation($title: String!, $usersIds: [ID!]!, $loggedUserId: ID!, $photoId: ID) {
+    createChat(
+      title: $title,
+      usersIds: $usersIds
+      isGroup: true,
+      photoId: $photoId
+    ) {
+      ...ChatFragment
+      ...ChatMessagesFragment
+    }
+  }
+  ${ChatFragment}
+  ${ChatMessagesFragment}
+`;
+
+export const USER_CHATS_SUBSCRIPTION = gql`
+  subscription UserChatsSubscription($loggedUserId: ID!) {
+    Chat(
+      filter: {
+        mutation_in: [ CREATED, UPDATED ],
+        node: {
+          users_some: {
+            id: $loggedUserId
+          }
+        }
+      }
+    ) {
+      mutation
+      node {
+        ...ChatFragment
+        ...ChatMessagesFragment
+      }
+    }
+  }
+  ${ChatFragment}
+  ${ChatMessagesFragment}
+`;
--- src/app/chat/services/chat.service.ts
+++ src/app/chat/services/chat.service.ts
@@ -1,10 +1,39 @@
 import { Injectable } from '@angular/core';
 import { ChatModule } from '../chat.module';
+import { Apollo, QueryRef } from 'apollo-angular';
+import { Observable } from 'rxjs';
+import { AllChatsQuery, USER_CHATS_QUERY } from './chat.graphql';
+import { Chat } from '../models/chat.model';
+import { AuthService } from '../../core/services/auth.service';
+import { Router } from '@angular/router';
+import { UserService } from '../../core/services/user.service';
+import { Subscription } from 'apollo-client/util/Observable';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: ChatModule
 })
 export class ChatService {
 
-  constructor() { }
+  chats$: Observable<Chat[]>;
+  private queryRef: QueryRef<AllChatsQuery>;
+  private subscriptions: Subscription[] = [];
+
+  constructor(
+    private apollo: Apollo,
+    private authService: AuthService,
+    private router: Router,
+    private userService: UserService
+  ) { }
+
+  getUserChats(): Observable<Chat[]> {
+    return this.apollo.query<AllChatsQuery>({
+      query: USER_CHATS_QUERY,
+      variables: {
+        userId: this.authService.authUser.id
+      }
+    }).pipe(
+      map(res => res.data.allChats)
+    );
+  }
 }

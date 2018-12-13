b2f43e09794b4fce529fb2dd00868f0606682ee5
Daniel Oliveira <danielso2007@gmail.com>
2018-12-13 15:55:00 -0200

    feat: Ordenando Chats pela última mensagem ou data de criação.

--- src/app/chat/services/chat.service.ts
+++ src/app/chat/services/chat.service.ts
@@ -31,7 +31,15 @@ export class ChatService {
         loggedUserId: this.authService.authUser ? this.authService.authUser.id : ''
       }
     }).pipe(
-      map(res => res.data.allChats)
+      map(res => res.data.allChats),
+      map((chats: Chat[]) => {
+        const chatsToSort = chats.slice();
+        return chatsToSort.sort((a, b) => {
+          const valueA = (a.messages.length > 0)  ? new Date(a.messages[0].createdAt).getTime() : new Date(a.createdAt).getTime();
+          const valueB = (b.messages.length > 0)  ? new Date(b.messages[0].createdAt).getTime() : new Date(b.createdAt).getTime();
+          return valueB - valueA;
+        });
+      })
     );
   }
 

59541f7bf004062fde2b0ce4f1a800d99bd83a67
Daniel Oliveira <danielso2007@gmail.com>
2018-12-13 14:32:58 -0200

    feat: Exibindo nome do usuário ou titulo do chat na toolbar.

--- src/app/chat/components/chat-window/chat-window.component.ts
+++ src/app/chat/components/chat-window/chat-window.component.ts
@@ -1,8 +1,11 @@
+import { Title } from '@angular/platform-browser';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Chat } from '../../models/chat.model';
 import { Subscription } from 'apollo-client/util/Observable';
-import { map, mergeMap, tap } from 'rxjs/operators';
+import { map, mergeMap, tap, take } from 'rxjs/operators';
+import { UserService } from '../../../core/services/user.service';
+import { User } from '../../../core/models/user.model';
 
 @Component({
   selector: 'app-chat-window',
@@ -16,26 +19,35 @@ export class ChatWindowComponent implements OnInit, OnDestroy {
   private subscriptions: Subscription[] = [];
 
   constructor(
-    private router: ActivatedRoute
+    private router: ActivatedRoute,
+    private title: Title,
+    private userService: UserService
   ) { }
 
   ngOnInit() {
-    this.router.data
-    .pipe(
-      map(routerData => this.chat = routerData.chat),
-      mergeMap(() => this.router.paramMap),
-      tap((params: ParamMap) => {
-        if (!this.chat) {
-          this.recipientId = params.get('id');
-          console.log('User id: ', this.recipientId);
-        }
-      })
-    )
-    .subscribe();
+    this.subscriptions.push(
+      this.router.data
+      .pipe(
+        map(routerData => this.chat = routerData.chat),
+        mergeMap(() => this.router.paramMap),
+        tap((params: ParamMap) => {
+          if (!this.chat) {
+            this.recipientId = params.get('id');
+            console.log('User id: ', this.recipientId);
+            this.userService.getUserById(this.recipientId)
+            .pipe(take(1)).subscribe((user: User) => this.title.setTitle(user.name));
+          } else {
+            this.title.setTitle(this.chat.title || this.chat.users[0].name);
+          }
+        })
+      )
+      .subscribe()
+    );
   }
 
   ngOnDestroy() {
     this.subscriptions.forEach(s => s.unsubscribe());
+    this.title.setTitle('Angular Graphcool Chat');
   }
 
 }
